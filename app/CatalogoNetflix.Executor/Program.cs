0cdb0a1f1754afa650a3049f7ab0c9c6dce2a674
Kevyn Carlos <kevyncarlos@gmail.com>
2020-10-09 13:47:08 -0300

    Leitura do CSV e conversão para JSON

---
+++ app/CatalogoNetflix.Executor/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
+
+namespace CatalogoNetflix.Executor
+{
+    class Program
+    {
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            List<object> catalogo = new List<object>();
+            string[] lines = File.ReadAllLines(@$"{Directory.GetCurrentDirectory()}\CSV\netflix.csv");
+            foreach (string line in lines.Skip(1))
+            {
+                string[] linhaSeparada = line.Split(";");
+                if (linhaSeparada.Length == 7)
+                {
+                    catalogo.Add(new
+                    {
+                        Title = linhaSeparada[0],
+                        Rating = linhaSeparada[1],
+                        RantingDescription = linhaSeparada[2],
+                        RatingLevel = linhaSeparada[3],
+                        ReleaseYear = linhaSeparada[4],
+                        UserRatingScore = linhaSeparada[5],
+                        UserRatingSize = linhaSeparada[6]
+                    });
+                }
+            }
+
+            var jsonString = JsonSerializer.Serialize(catalogo);
+        }
+    }
+}
